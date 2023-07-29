384ba8bdeb65e32bd425fb9ea3ca147682e44bbc
Vinimolz <76082483+Vinimolz@users.noreply.github.com>
2023-07-28 19:04:26 -0700

    Added few interview questions to main.py

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from Linked_List import *
+def two_sum(nums, target):
+    for i in range(len(nums) - 1):
+        wanted = target - nums[i]
+        for j in range(i + 1, len(nums)):
+            print(len(nums))
+            if nums[j] == wanted:
+                return [i, j]
+
+
+def twoSum(nums, target):
+    seen_nums = {}
+
+    for i in range(len(nums) - 1):
+        wanted_match = target - nums[i]
+
+        if wanted_match in seen_nums:
+            return[seen_nums[wanted_match], i]
+
+        else:
+            seen_nums[nums[i]] = i
+
+    print(seen_nums)
+    return []
 
 
 def main():
-    my_linked_list = LinkedList()
-    my_linked_list.insert_node(2)
-    my_linked_list.print()
+    nums = [2, 7, 11, 15]
+    target = 9
+
+    print(twoSum(nums, target))
 
 
 main()
